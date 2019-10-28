51c627d3305c4ae0e8ad0eff6bc26ab344f45523
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2019-10-28 02:29:26 -0300

    Configurando o  nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '6382796eb5aafd',
+        pass: 'd26c52ed7f5595',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com',
+    },
+};
